7ebe02b0333002f8de0fa4ea3829e9f24e7daa6b
Ian Rogers <ian.rogers@contactclean.com>
2010-09-25 01:51:44 +0000

    Added simple test script to see if all the "uses" files compile

---
+++ v15.6.4/test/test_uses_compile.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# export usepop=$(readlink `dirname $0`/..)
+export usepop=/usr/share/poplog/current-poplog
+. $usepop/bin/poplog.sh
+
+mkdir -p /mnt/tmp
+T=/mnt/tmp/test_uses_compile.$$
+
+echo '<?xml version="1.0"?>'
+echo '<testsuite name="test_uses_compile">'
+
+print_case () {
+	class=$1
+	name=$2
+	type=$3
+
+	echo "<testcase classname=\"$class\" name=\"$name\">"
+	echo "    <$type message=\"$name\" line=\"0\">"
+	if [[ $type == 'failure' ]] ; then
+		echo "<![CDATA["
+		cat $T
+		echo "]]>"
+	else
+		echo $file
+	fi
+	echo "    </$type>"
+	echo "</testcase>"
+}
+
+execute_test () {
+	class=$1
+	name=$2
+	cmd=$3
+	shift; shift; shift
+	$cmd "$*" < /dev/null > $T 2>&1
+	if [[ `grep -c MISHAP $T` -gt 0 ]] ; then
+		print_case $class $name 'failure' 
+	else 
+		print_case $class $name 'info' 
+	fi
+}
+
+# do the standard libraries compile?
+for d in $poplocalauto $popautolib $popvedlib $usepop/pop/lib/database $poplocal/local/lib $usepop/pop/packages/lib $popliblib $popdatalib ; do
+	if [[ -d $d ]] ; then
+		for f in $d/*.p ; do
+			f=$(basename $f)
+			f=${f%.*}
+			execute_test $d $f pop11 ":uses $f"
+		done
+	fi
+done
+
+echo '</testsuite>'
